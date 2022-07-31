c88d8f0b0c9b17babdee1b9445e2e0049b80d0b0
Naman Vaishnav <namanmvaishnav@gmail.com>
2022-07-31 14:00:17 +0530

    add advance animation on annotation
    - overlay lat long on top of map

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -25,12 +25,60 @@ struct MapView: View {
             
 //            MapPin(coordinate: item.location, tint: .accentColor)
 //            MapMarker(coordinate: item.location, tint: .accentColor)
+//            MapAnnotation(coordinate: item.location) {
+//                Image("logo")
+//                    .resizable()
+//                    .frame(width: 32, height: 32, alignment: .center)
+//            }//: ANNOTATION
+            
             MapAnnotation(coordinate: item.location) {
-                Image("logo")
+                MapAnnotationView(location: item)
+            }
+            
+        }) // : MAP
+        .overlay(
+            HStack (alignment: .center, spacing: 12){
+                Image("compass")
                     .resizable()
-                    .frame(width: 32, height: 32, alignment: .center)
-            }//: ANNOTATION
-        })
+                    .scaledToFill()
+                .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                    
+                )
+                .padding()
+            ,alignment:  .top
+        )
+        .ignoresSafeArea()
     }
 }
 
