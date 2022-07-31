10faf5166c1ee6fa8968a32de4d76c8b2d788cee
Naman Vaishnav <namanmvaishnav@gmail.com>
2022-07-31 11:57:23 +0530

    add completion of animalDetailView

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -49,16 +49,37 @@ struct AnimalDetailView: View {
                 .padding(.horizontal)
                 
                 // FACTS
+                Group {
+                    HeadingView(headingImage: "questionmark.circle", headingText: "Did you Know?")
+                    InsetFactView(animal: animal)
+                }
+                .padding(.horizontal)
                 
                 // DESCRIPTION
+                Group {
+                    HeadingView(headingImage: "info.circle", headingText: "All about \(animal.name)")
+                    Text(animal.description)
+                        .multilineTextAlignment(.leading)
+                        .layoutPriority(1)
+                }
+                .padding(.horizontal)
                 
                 // MAP
+                Group{
+                    HeadingView(headingImage: "map", headingText: "National Parks")
+                    InsetMapView()
+                }
+                .padding(.horizontal)
                 
                 // LINK
+                Group{
+                    HeadingView(headingImage: "books.vertical", headingText: "Learn more")
+                    ExternalWebLinkView(animal: animal)
+                }
                 
             }// : VSTACK
             .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
-        }
+        }//: SCROLL
     }
 }
 
