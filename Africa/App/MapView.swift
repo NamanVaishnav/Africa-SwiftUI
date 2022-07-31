c67ea6c7477448e5a04ea8f38bfe18c55bd86358
Naman Vaishnav <namanmvaishnav@gmail.com>
2022-07-31 13:43:42 +0530

    add map and custom annotation

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,31 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    //MARK: - PROPERTIES
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent:  {
+            item in
+            
+//            MapPin(coordinate: item.location, tint: .accentColor)
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }//: ANNOTATION
+        })
     }
 }
 
---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Naman Vaishnav on 31/07/22.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    let id: String
+    let name: String
+    let image: String
+    let latitude: Double
+    let longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
